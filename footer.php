6aae3c91f5d46320f224cd1d0b45c6baf20f54cd
Clare Liguori <clare.liguori@gmail.com>
2010-07-13 02:23:22 +0000

    Start over on the trunk!

--- footer.php
+++
@@ -1,9 +0,0 @@
-</div>
-
-<div id="footer">
-If you have any problems with this site, please report it to <a href="mailto: <?php echo $webmaster_email; ?>"><?php echo $webmaster_email; ?></a>.
-</div>
-
-
-</body>
-</html>
--- showfile.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include 'lib.php';
-
-if(check_secretariat_session()) {
-  $file_lines = read_file($_GET['filename']);
-  echo implode("\n",$file_lines);
-}
- ?>
