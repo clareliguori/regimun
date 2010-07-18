e37baba90045eff89e75c436e64967cf1e74356a
Clare Liguori <clare.liguori@gmail.com>
2010-07-18 19:02:24 +0000

    use generic views

--- django_regimun/regimun_app/urls.py
+++ django_regimun/regimun_app/urls.py
@@ -1,13 +1,27 @@
 from django.conf.urls.defaults import *
+from regimun_app.models import Conference
 
-urlpatterns = patterns('django_regimun.regimun_app.views',
+conferences = Conference.objects.all()
 
-    (r'^$', 'conferences_index'),
-    (r'^(?P<conference_slug>[-\w]+)/$', 'schools_index'),
-    (r'^(?P<conference_slug>[-\w]+)/secretariat/$', 'conference_admin'),
-    (r'^(?P<conference_slug>[-\w]+)/(?P<school_slug>[-\w]+)/$', 'school_admin'),
+urlpatterns = patterns('',   #'django_regimun.regimun_app.views',
+    # conferences index
+    (r'^$', 'django.views.generic.list_detail.object_list', dict(queryset=conferences)),
+    
+    # schools index
+    (r'^(?P<slug>[-\w]+)/$',
+        'django.views.generic.list_detail.object_detail',
+        dict(queryset=conferences, slug_field='url_name')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # secretariat admin page
+    (r'^(?P<slug>[-\w]+)/secretariat/$',
+        'django.views.generic.list_detail.object_detail',
+        dict(queryset=conferences, slug_field='url_name', template_name='regimun_app/secretariat/index.html')),
+
+    # register new school
+    (r'^(?P<slug>[-\w]+)/new-school/$',
+        'django.views.generic.list_detail.object_detail',
+        dict(queryset=conferences, slug_field='url_name', template_name='regimun_app/register-new-school.html')),
+    
+    (r'^(?P<conference_slug>[-\w]+)/(?P<school_slug>[-\w]+)/$', 'django_regimun.regimun_app.views.school_admin'),
+
 )
