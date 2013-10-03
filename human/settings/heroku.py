d1a0fcf45cb99dccc356747fab31cb87ab6ed7d9
Jason Novinger <jnovinger@gmail.com>
2013-10-03 13:51:17 -0500

    Straighten out where exactly our static/ dir will live

--- human/settings/heroku.py
+++ human/settings/heroku.py
@@ -15,7 +15,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
--- human/settings/settings.py
+++ human/settings/settings.py
@@ -1,7 +1,10 @@
-# Django settings for human project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+# two levels up ...
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -61,7 +64,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
