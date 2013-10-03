3571b04a324856bab54cc2bd7da7439ae924ae19
Jason Novinger <jnovinger@gmail.com>
2013-10-03 11:16:13 -0500

    Update to match Heroku's suggested setup

--- human/heroku.py
+++ human/heroku.py
@@ -1,24 +1,27 @@
 import os
 import urlparse
 
+import dj_database_url
+
 from .settings import *
 
-@property
-def DATABASES():
-    """Setup DATABASES setting, but hide intermediate bits"""
-    url = urlparse.urlparse(os.environ['DATABASE_URL'])
+# Parse database configuration from $DATABASE_URL
+DATABASES['default'] =  dj_database_url.config()
 
-    db = {
-        'NAME': url.path[1:],
-        'USER': url.username,
-        'PASSWORD': url.password,
-        'HOST': url.hostname,
-        'PORT': url.port,
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    }
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-    return { 'default': db }
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
+# Static asset configuration
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 @property
 def DEBUG():
     """Set DEBUG based on env vars"""
