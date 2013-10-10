3973f22a1931ee176852b3e1a55c10f2bdd753af
Jason Novinger <jnovinger@gmail.com>
2013-10-10 13:12:51 -0500

    Refs #3: make sure Django is using the core 'request' context processor so that {{ request.* }} references don't fail in templates.

--- human/settings/settings.py
+++ human/settings/settings.py
@@ -146,6 +146,17 @@ EMAIL_PORT = 587
 # turns out, this is important to regimun_app
 LOGIN_REDIRECT_URL = '/'
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.request',
+)
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
